e1f788b7b0a167adb4218c9d28cbf426400548de
Cadbury <abdul.thomso.com>
2020-03-11 22:05:43 +0530

    mainHeader in component

---
+++ frontend/src/components/Headers/MainHeader.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class MainHeader extends Component {
+    // state = {  }
+    render() { 
+        return ( <div>THis is the MainHeader</div> );
+    }
+}
+ 
+export default MainHeader;
